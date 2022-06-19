830675734af58d5e8575b12ea64e09d05d7cca32
Kamil Przebieracz <dmjs23@gmail.com>
2022-06-19 12:21:01 +0200

    update

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,3 +1,15 @@
-const FeedBack = () => {
-    
+export const FeedBack = () => {
+    return (
+        <>
+            <span className="licznik_like"></span>
+            <span className="licznik_neutral"></span>
+            <span className="licznik_dislike"></span>
+            <button>Like</button>
+            <button>neutral</button>
+            <button>dislike</button>
+        </>
+    )
 }
+
+
+export default FeedBack;
